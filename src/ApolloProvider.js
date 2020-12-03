85d4d0b725c1a7da8397d245dc2d83433dd5b1b0
kqpham <kevinqpham22@gmail.com>
2020-12-03 12:46:44 -0500

    edit proxy and server base url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -8,7 +8,7 @@ import { setContext} from 'apollo-link-context';
 
 
 const httpLink = createHttpLink ({
-    uri: "http://localhost:5000"
+    uri: `https://fathomless-falls-79499.herokuapp.com/`
 });
 
 const authLink = setContext(() => {
